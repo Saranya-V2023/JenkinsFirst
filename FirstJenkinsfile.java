9435878c86f2cc1d39727e93cee6a70cf2dee475
Saranya-V2023 <122074818+Saranya-V2023@users.noreply.github.com>
2023-01-19 12:08:46 +0530

    Add files via upload

---
+++ FirstJenkinsfile.java
@@ -0,0 +1,7 @@
+public class FirstJenkinsfile{
+
+public static void main(String args[]){
+System.out.println("where yor are in ???");
+System.out.println("I am in native ,currently in LTIMindtree ,");
+}
+}
